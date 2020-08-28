636c85478b398a58367c29a753bb114d57c06782
SeungminKim <ksm0057@naver.com>
2020-08-28 21:29:05 +0900

    error

--- src/App2.js
+++ src/App2.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the section 2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
 
 const App2 = () => {
-  return <h1>hello</h1>;
+  const { currentItem } = useTabs(0, content);
+  return (
+    <div className="App2">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
 };
 
 export default App2;
