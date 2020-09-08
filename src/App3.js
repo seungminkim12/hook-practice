f1f2cae8b2d38c6cc499084387f9035beaddf243
SeungminKim <ksm0057@naver.com>
2020-09-08 16:05:05 +0900

    hook by setTitle

--- src/App3.js
+++ src/App3.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const App3 = () => {
+/*const App3 = () => {
   const [number, setNumber] = useState(0);
   const [anumber, setANumber] = useState(0);
   const sayHello = () => {
@@ -14,6 +14,26 @@ const App3 = () => {
       <h1>Hi</h1>
       <button onClick={() => setNumber(number + 1)}>{number}</button>
       <button onClick={() => setANumber(anumber + 1)}>{anumber}</button>
+    </div>
+  );
+};*/
+
+const useTitle = (value) => {
+  const [title, setTitle] = useState(value);
+  const getTitle = () => {
+    const tagTitle = document.querySelector("title");
+    tagTitle.innerText = title;
+  };
+  useEffect(getTitle, [title]);
+  return setTitle;
+};
+
+const App3 = () => {
+  const oldTitle = useTitle("Loadddding");
+  setTimeout(() => oldTitle("home"), 5000);
+  return (
+    <div>
+      <h1>hello</h1>
     </div>
   );
 };
